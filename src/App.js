311d775895ec9044d7903ff08aa125e3969f6efd
mfranl <mflandry09@gmail.com>
2021-03-31 13:00:55 -0600

    fixing components

--- src/App.js
+++ src/App.js
@@ -1,33 +1,40 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Search from "./pages/Search";
-import Navbar from "./components/NavBar/NavBar"
-import './App.css';
+import React, { useState } from 'react';
+import Layout from "./components/Layout";
+import Header from "./components/Header";
+import SearchBar from "./components/SearchBar";
+import employee from "./data/employee.json";
+import EmployeeInfo from './components/EmployeeInfo';
 
 function App() {
-  return (
-    <Router>
-      <>
-      
-      <Navbar />
-      <Route exact path="/employee-tracker"component={Home} />
-      <Route exact path="/home"component={Home} />
-      <Route exact path="/search"component={Search} />
-      
-      
-      
-      
-      
-      </>
+    const [ searchTerm, setSearchTerm ] = useState("");
+    const [ sort, alphabetical] = useState(false);
+    const [ data, setEmployee ] = useState(employee);
 
+    function handleSearchTerm (event) {
+        setSearchTerm(event.target.value)
+    }
 
+    function nameSort(){
+        setEmployee(data.sort((a,b) => (a.name > b.name)
+        ))
+        alphabetical(true);
+    }
+    const filteredEmployees = data.filter(employee => employee.name.toLowerCase().startsWith(searchTerm.toLowerCase()));
 
+    return (
+        <div>
+            <Header/>
+            <Layout>
+                    <SearchBar 
+                    onSearch={handleSearchTerm}
+                    searchTerm={searchTerm}
+                    nameSort={nameSort}
+                    />
+                <EmployeeInfo data={filteredEmployees}/>
+            </Layout>
+        </div>
+    )
 
-
-
-    </Router>
-  )
 }
 
 export default App;
